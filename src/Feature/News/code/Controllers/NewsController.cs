2ed4f3fdaee5d87d3292aa5917179054ff539029
Neil Shack <nshack31@hotmail.com>
2016-11-10 22:17:48 +0000

    Glass NewsList (rough sample)

--- src/Feature/News/code/Controllers/NewsController.cs
+++ src/Feature/News/code/Controllers/NewsController.cs
@@ -1,35 +1,42 @@
 ﻿namespace Sitecore.Feature.News.Controllers
 {
-  using System.Web.Mvc;
-  using Sitecore.Feature.News.Repositories;
-  using Sitecore.Foundation.SitecoreExtensions.Extensions;
-  using Sitecore.Mvc.Presentation;
+    using System.Web.Mvc;
+    using Sitecore.Feature.News.Repositories;
+    using Sitecore.Foundation.SitecoreExtensions.Extensions;
+    using Sitecore.Mvc.Presentation;
+    using Models;
+    using Glass.Mapper.Sc.Web.Mvc;
+    using Glass.Mapper.Sc;
 
-  public class NewsController : Controller
-  {
-    private readonly INewsRepositoryFactory newsRepositoryFactory;
-
-    public NewsController() : this(new NewsRepositoryFactory())
+    public class NewsController : GlassController
     {
-    }
+        private readonly INewsRepositoryFactory newsRepositoryFactory;
 
-    public NewsController(INewsRepositoryFactory newsRepositoryFactory)
-    {
-      this.newsRepositoryFactory = newsRepositoryFactory;
-    }
+        public NewsController() : this(new NewsRepositoryFactory())
+        {
+        }
 
-    public ActionResult NewsList()
-    {
-      var items = this.newsRepositoryFactory.Create(RenderingContext.Current.Rendering.Item).Get();
-      return this.View("NewsList", items);
-    }
+        public NewsController(INewsRepositoryFactory newsRepositoryFactory)
+        {
+            this.newsRepositoryFactory = newsRepositoryFactory;
+        }
 
-    public ActionResult LatestNews()
-    {
-      //TODO: change to parameter template
-      var count = RenderingContext.Current.Rendering.GetIntegerParameter("count", 5);
-      var items = this.newsRepositoryFactory.Create(RenderingContext.Current.Rendering.Item).GetLatestNews(count);
-      return this.View("LatestNews", items);
+        public ActionResult NewsList()
+        {
+            ISitecoreContext context = new SitecoreContext();
+            //var repo = newsRepositoryFactory.Create(RenderingContext.Current.ContextItem);
+
+            var model = context.QueryRelative<INewsArticle>(".//*[@@templatename='News Article']");
+
+            return View("NewsList", model);
+        }
+
+        public ActionResult LatestNews()
+        {
+            //TODO: change to parameter template
+            var count = RenderingContext.Current.Rendering.GetIntegerParameter("count", 5);
+            var items = this.newsRepositoryFactory.Create(RenderingContext.Current.Rendering.Item).GetLatestNews(count);
+            return this.View("LatestNews", items);
+        }
     }
-  }
 }
---
+++ src/Feature/News/code/Models/INewsArticle.cs
@@ -0,0 +1,33 @@
+﻿using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using System;
+
+namespace Sitecore.Feature.News.Models
+{
+    [SitecoreType(TemplateId = Templates.NewsArticle.TemplateID)]
+    public interface INewsArticle
+    {
+        [SitecoreId]
+        Guid ID { get; set; }
+
+        [SitecoreField]
+        string NewsTitle { get; set; }
+
+        [SitecoreField]
+        DateTime NewsDate { get; set; }
+
+        [SitecoreField]
+        Image NewsImage { get; set; }
+
+        [SitecoreField]
+        string NewsSummary { get; set; }
+
+        [SitecoreField]
+        string NewsBody { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Url)]
+        string Url { get; set; }
+
+    }
+}
