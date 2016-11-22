14461863b22298328280069ef0ed78eda3bf452c
Mike Edwards <mike@glass.lu>
2016-11-22 22:09:26 +0000

    Refactored news xcshtml files and improved GlassMappings

--- src/Feature/News/code/App_Start/GlassMappings.cs
+++ src/Feature/News/code/App_Start/GlassMappings.cs
@@ -9,13 +9,14 @@
         public override void Configure()
         {
             this.Map(
-                x => x.TemplateId(Templates.NewsArticle.ID),
+                x => x.TemplateId(Templates.NewsArticle.ID)
+                    .EnforceTemplateAndBase(),
                 x => x.Id(y=>y.ID),
-                x => x.Field(y => y.NewsTitle),
-                x => x.Field(y => y.NewsDate),
-                x => x.Field(y => y.NewsImage),
-                x => x.Field(y => y.NewsSummary),
-                x => x.Field(y => y.NewsBody),
+                x => x.Field(y => y.NewsTitle).FieldId(Templates.NewsArticle.Fields.Title),
+                x => x.Field(y => y.NewsDate).FieldId(Templates.NewsArticle.Fields.Date),
+                x => x.Field(y => y.NewsImage).FieldId(Templates.NewsArticle.Fields.Image),
+                x => x.Field(y => y.NewsSummary).FieldId(Templates.NewsArticle.Fields.Summary),
+                x => x.Field(y => y.NewsBody).FieldId(Templates.NewsArticle.Fields.Body),
                 x => x.Info(y => y.Url).InfoType(SitecoreInfoType.Url)
                 );
         }
